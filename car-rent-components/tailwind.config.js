da66180dac014dc8100d5b1ff5862646da878945
Twiyubake Innovation <brother.dushime@gmail.com>
2024-10-15 20:10:53 +0200

    initialized car rent system components design

---
+++ car-rent-components/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          primary: "#000"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
