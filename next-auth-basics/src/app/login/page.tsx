3c8e76eb8029159df63ec14883668e1e642256a9
Dushime Brother <dushimeconso1234@gmail.com>
2024-10-17 23:50:26 +0200

    update the login page

--- next-auth-basics/src/app/login/page.tsx
+++ next-auth-basics/src/app/login/page.tsx
@@ -8,16 +8,16 @@ const page = () => {
                <div  className="w-[40%]  py-[40px] shadow-md rounded-sm shadow-gray-400 flex flex-col items-center p-[10px] gap-[10px] justify-center">
                     <h3>Login</h3>
                     <div onClick={() => signIn('google') } className='w-[80%] cursor-pointer bg-blue-500 flex items-center  justify-start rounded-[5px] p-[5px] '> 
-                         <p>Continue With Google</p> 
+                         <p className=' text-white text-[0.8rem] font-bold'>Continue With Google</p> 
                     </div>
                     <div onClick={() => signIn('apple') } className='w-[80%] cursor-pointer bg-gray-600 flex items-center  justify-start rounded-[5px] p-[5px] '> 
-                         <p>Continue With Apple</p>
+                         <p className=' text-white text-[0.8rem] font-bold'>Continue With Apple</p>
                     </div>
                     <div onClick={() => signIn('azure-ad') } className='w-[80%] cursor-pointer bg-gradient-to-r from-red-500 via-orange-400 to-green-600  justify-start rounded-[5px] p-[5px] '> 
-                         <p>Continue With Microsoft</p> 
+                         <p className=' text-white text-[0.8rem] font-bold'>Continue With Microsoft</p> 
                     </div>
                     <div onClick={() => signIn('facebook') } className='w-[80%] cursor-pointer bg-blue-500 flex items-center  justify-start rounded-[5px] p-[5px] '> 
-                         <p>Continue With Facebook</p> 
+                         <p className=' text-white text-[0.8rem] font-bold'>Continue With Facebook</p> 
                     </div>
                </div>
           </div>
